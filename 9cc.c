a5277f10ff218308d274c9f3251fb8d0b95a8237
Shuhei Kiyosawa <nicepass427@gmail.com>
2020-04-25 05:38:43 +0900

    add tokenize, recursive descent parser, stach machine

--- 9cc.c
+++ 9cc.c
@@ -5,7 +5,7 @@
 #include<stdlib.h>
 #include<string.h>
 
-typedef enum{
+typedef enum {
 	TK_RESERVED,
 	TK_NUM,
 	TK_EOF
@@ -84,6 +84,16 @@ Token *tokenize(char *p) {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
+
+		if (*p == '*' || *p == '/') {
+			cur = new_token(TK_RESERVED, cur, p++);
+			continue;
+		}
+
+		if (*p == '(' || *p == ')') {
+			cur = new_token(TK_RESERVED, cur, p++);
+			continue;
+		}
 		
 		if (isdigit(*p)) {
 			cur = new_token(TK_NUM, cur, p);
@@ -97,7 +107,113 @@ Token *tokenize(char *p) {
 	new_token(TK_EOF, cur, p);
 	return head.next;
 }
-			
+
+typedef enum {
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr();
+
+Node *primary() {
+	
+	if (consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+
+	return new_node_num(expect_number());
+}
+
+Node *mul() {
+	Node *node = primary();
+
+	for(;;){
+		if(consume('*')) 
+			node = new_node(ND_MUL, node, primary());
+		else if(consume('/'))
+			node = new_node(ND_DIV, node, primary());
+		else 
+			return node;
+	}
+}
+
+Node *expr() {
+	Node *node = mul();
+
+	for(;;) {
+		if(consume('+'))
+			node = new_node(ND_ADD, node, mul());
+		else if(consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+void gen(Node *node) {
+	if(node->kind == ND_NUM) {
+		//push
+		printf("	mov	x8, %d\n", node->val);
+		printf("	str	x8, [sp, 16]!\n");
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+	
+	//pop
+	printf("	ldr	w9, [sp], -16\n");
+	//pop
+	printf("	ldr	w8, [sp], -16\n");
+
+	switch(node->kind) {
+		case ND_ADD: 
+			printf("	add	x8, x8, x9\n");
+			break;
+		case ND_SUB:
+			printf("	sub	x8, x8, x9\n");
+			break;
+		case ND_MUL:
+			printf("	mul	x8, x8, x9\n");
+			break;
+		case ND_DIV:
+			printf("	sdiv	x8, x8, x9\n");
+			break;
+	}
+	
+	//push
+	printf("	str	x8, [sp, 16]!\n");
+}
+
 
 int main( int argc, char **argv) {
 	if(argc != 2) {
@@ -105,7 +221,8 @@ int main( int argc, char **argv) {
 		return 1;
 	}
 	user_input = argv[1];
-	token = tokenize(argv[1]);
+	token = tokenize(user_input);
+	Node *node = expr();
 
 	printf("	.arch armv8-a\n");
 	printf("	.text\n");
@@ -115,18 +232,9 @@ int main( int argc, char **argv) {
 	printf("	.type	main, %%function\n");
 	printf("main:\n");
 	
-	printf("	mov	w0, %d\n", expect_number());
+	gen(node);
 
-	while(!at_eof()) {
-		if(consume('+')) {
-			printf("	add	w0, w0, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("	sub	w0, w0, %d\n", expect_number());
-	}
-
+	printf("	ldr	x0, [sp], -16\n");
 	printf("	ret\n");
 	return 0;
 }
--- test.sh
+++ test.sh
@@ -20,5 +20,9 @@ assert 6 "6"
 assert 1 "1" 
 assert 4 "1+6-3"
 assert 41 " 12 + 34 - 5 "
+assert 30 " 6 * 5"
+assert 5 "10 / 2"
+assert 40 "10 / 2 + 5 * 7"
+assert 30 "(2 + 1) * 10"
 
 echo OK
